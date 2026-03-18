be4e3a05b58a877fc713cced206aaf5e9fd395d0
Akshat Talwar <uniquedimension1@gmail.com>
2026-03-18 10:33:53 +0530

    database load

--- database.py
+++ database.py
@@ -10,10 +10,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
-
 class Base(DeclarativeBase):
     pass
-
 
 async def get_db():
     async with SessionLocal() as session:
