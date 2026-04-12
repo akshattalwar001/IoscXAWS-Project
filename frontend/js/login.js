8b8d5e2ba5ada12aac3d9d79340d36e7ed8cb6cf
Akshat Talwar <uniquedimension1@gmail.com>
2026-04-12 13:53:02 +0530

    fix login

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Environment-based API determination
   const API_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') 
     ? "http://localhost:8000" 
-    : "https://estudent-cell.onrender.com";
+    : "http://213.210.37.18";
 
   // ----------------------------------------------------
   // Pre-fill Remember Me
